64406ad0a3587286b7e758718dfd1e0a6e370d31
Sean Nealon <signalflowsean@gmail.com>
2018-10-19 12:46:25 -0400

    test for post note should reutnr an error when missing title field is working

--- test/server.test.js
+++ test/server.test.js
@@ -85,78 +85,92 @@ describe('Noteful API', function() {
       return chai
         .request(app)
         .get('/api/notes')
-        .then(function(res){ 
+        .then(function(res) {
           expect(res.body).to.be.a('array');
-          res.body.forEach(function(item) { 
-            expect(item).to.be.a('object'); 
-            expect(item).to.contain.keys(
-              'id', 'title', 'content'
-            ); 
-          });  
-        }); 
+          res.body.forEach(function(item) {
+            expect(item).to.be.a('object');
+            expect(item).to.contain.keys('id', 'title', 'content');
+          });
+        });
     });
 
     it('should return an empty array for an incorrect searchTerm', function() {
       return chai
         .request(app)
         .get('/api/notes?searchTerm=pizza')
-        .then(function(res){ 
-          expect(res.body).to.be.a('array'); 
-          expect(res.body).to.have.length(0); 
-        }); 
+        .then(function(res) {
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(0);
+        });
     });
   });
 
-  describe('GET /api/notes/:id', function () {
-
-    it('should return correct note when given an id', function () {
-      const newItem = { id: 2,
+  describe('GET /api/notes/:id', function() {
+    it('should return correct note when given an id', function() {
+      const newItem = {
+        id: 2,
         title: 'There is no greater love',
         content: 'Can there be no greater thing than cheese$1',
         folderId: 1,
         folderName: 'Archive',
         tagId: 3,
-        tagName: 'BOXING' };  
+        tagName: 'BOXING'
+      };
       return chai
         .request(app)
         .get('/api/notes/2')
-        .then(function(res){ 
-          expect(res.body).to.contain.keys('id','title', 'content'); 
-          expect(res.body.title).to.deep.equal(newItem.title); 
-          expect(res.body.content).to.deep.equal(newItem.content); 
-        }); 
+        .then(function(res) {
+          expect(res.body).to.contain.keys('id', 'title', 'content');
+          expect(res.body.title).to.deep.equal(newItem.title);
+          expect(res.body.content).to.deep.equal(newItem.content);
+        });
     });
 
-    it('should respond with a 404 for an invalid id', function () {
+    it('should respond with a 404 for an invalid id', function() {
       return chai
         .request(app)
         .get('/api/notes/1000000000')
-        .then(function(res){ 
-          expect(res).to.have.status(404); 
-        }); 
+        .then(function(res) {
+          expect(res).to.have.status(404);
+        });
     });
-
   });
 
   describe('POST /api/notes', function() {
     const newItem = {
-      'title' : 'have a very spooky cherry tree', 
-      'content' : 'spark the thought', 
-      'folder_id' : 1 ,
-      'tags' : []
-    }; 
- 
+      title: 'have a very spooky cherry tree',
+      content: 'spark the thought',
+      folder_id: 1,
+      tags: []
+    };
+
     it('should create and return a new item when provided valid data', function() {
       return chai
         .request(app)
         .post('/api/notes')
         .send(newItem)
-        .then(function(res){ 
-          expect(res).to.have.status(201); 
-        }); 
+        .then(function(res) {
+          expect(res).to.have.status(201);
+        });
     });
 
-    //it('should return an error when missing "title" field', function() {});
+    it('should return an error when missing "title" field', function() {
+      const newItem = {
+        content: 'Can there be no greater thing than cheese$1',
+        folderId: 1,
+        folderName: 'Archive',
+        tagId: 3,
+        tagName: 'BOXING'
+      };
+      return chai
+        .request(app)
+        .post('/api/notes')
+        .send(newItem)
+        .then(function(res) { 
+          expect(res).to.have.status(400); 
+        }); 
+    }); 
+  
   });
 
   // describe('PUT /api/notes/:id', function() {
